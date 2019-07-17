d988a9e46d32c1cd1bf30376f3e853dae35e09b5
Robert Metcalf <rjmetcal@googlemail.com>
2019-07-17 14:41:11 +0100

    Adding AWS python library

--- object_store_abstraction/objectStores_DynamoDB.py
+++ object_store_abstraction/objectStores_DynamoDB.py
@@ -1,6 +1,7 @@
-from .objectStores_base import ObjectStore, ObjectStoreConnectionContext, StoringNoneObjectAfterUpdateOperationException, WrongObjectVersionException, TriedToDeleteMissingObjectException, TryingToCreateExistingObjectException, SuppliedObjectVersionWhenCreatingException
+from .objectStores_base import ObjectStore, ObjectStoreConnectionContext, StoringNoneObjectAfterUpdateOperationException, WrongObjectVersionException, TriedToDeleteMissingObjectException, TryingToCreateExistingObjectException, SuppliedObjectVersionWhenCreatingException, ObjectStoreConfigError
 from .paginatedResult import getPaginatedResult
 
+from boto3 import Session as AWSSession
 
 class ConnectionContext(ObjectStoreConnectionContext):
   objectStore = None
@@ -18,8 +19,34 @@ class ConnectionContext(ObjectStoreConnectionContext):
 
 # Class that will store objects
 class ObjectStore_DynamoDB(ObjectStore):
+  awsSession = None
+  objectPrefix = None
+
   def __init__(self, configJSON, externalFns, detailLogging, type):
     super(ObjectStore_DynamoDB, self).__init__(externalFns, detailLogging, type)
+
+    requiredConfigItems = ['aws_access_key_id','aws_secret_access_key','region_name','endpoint_url']
+    for x in requiredConfigItems:
+      if x not in configJSON:
+        raise ObjectStoreConfigError("APIAPP_OBJECTSTORECONFIG DynamoDB ERROR - config param " + x + " missing")
+    endpointURL = configJSON["endpoint_url"]
+    if endpointURL.strip().upper() == "NONE":
+      endpointURL = None
+
+    self.awsSession = AWSSession(
+      aws_access_key_id=configJSON["aws_access_key_id"],
+      aws_secret_access_key=configJSON["aws_secret_access_key"],
+      aws_session_token=None,
+      region_name=configJSON["region_name"],
+      botocore_session=None,
+      profile_name=None
+    )
+    if "objectPrefix" in ConfigDict:
+      self.objectPrefix = ConfigDict["objectPrefix"]
+    else:
+      self.objectPrefix = ""
+
+
     #Dict = (objDICT, objectVersion, creationDate, lastUpdateDate)
 
   def _resetDataForTest(self):
--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ sp_install_requires = [
   'pyjwt==1.7.1',
   'sqlalchemy==1.3.1',
   'PyMySQL==0.9.3',
-  'python-dateutil==2.7.2'
+  'python-dateutil==2.7.2',
+  'boto3==1.9.188'
 ]
 sp_tests_require = [
   'nose==1.3.7',
